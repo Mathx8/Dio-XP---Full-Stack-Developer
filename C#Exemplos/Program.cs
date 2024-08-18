74df6f0672cba526389c43a45eb3ce1bb95b7335
Mathx8 <matheusqueirozdev@gmail.com>
2024-08-18 15:42:29 -0300

    Exemplos

--- C#Exemplos/Program.cs
+++ C#Exemplos/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using C_Exemplos.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Buta";
+pessoa1.Idade = 26;
+pessoa1.Apresentar();
--- C#Exemplos/obj/Debug/net8.0/C#Exemplos.AssemblyInfo.cs
+++ C#Exemplos/obj/Debug/net8.0/C#Exemplos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#Exemplos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc4b9afeb2162abb2fc120a8c38c76a10f4b531")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e0be46f03a4c933090bedf63ca3edee15b0d24")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#Exemplos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#Exemplos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
