9ffe1214694fe58d69f89cb9d2c05a9c0cfcc742
Mathx8 <matheusqueirozdev@gmail.com>
2024-09-30 14:31:23 -0300

    Propriedades, métodos e construtores

---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,20 @@
+﻿using ExemploExplorando.Models;
+
+/*
+Pessoa p1 = new Pessoa();
+p1.Nome = "Leonardo";
+p1.Sobrenome = "Buta";
+p1.Idade = 20;
+p1.Apresentar();
+*/
+
+Pessoa p1 = new Pessoa("Leonardo", "Buta");
+Pessoa p2 = new Pessoa("Eduardo", "Neves Queiroz");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
