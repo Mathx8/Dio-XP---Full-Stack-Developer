7d47494ec7f1f036a63022584a4ec1e305cd4e37
Mathx8 <matheusqueirozdev@gmail.com>
2024-10-26 13:10:30 -0300

    inicio react

--- DesafioTDD/NewTalents/obj/Debug/net8.0/NewTalents.AssemblyInfo.cs
+++ DesafioTDD/NewTalents/obj/Debug/net8.0/NewTalents.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewTalents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5ae0fc74dccb58bd0b886953ea352efe46f6740")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd22beb2183ec5bac092cb341ec9518163e097d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewTalents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewTalents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
