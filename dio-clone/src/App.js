d65e4b382c7e74273c6f41e69b0a6c1fc3fb8c50
Mathx8 <matheusqueirozdev@gmail.com>
2024-11-02 14:13:52 -0300

    Dio Clone

--- dio-clone/src/App.js
+++ dio-clone/src/App.js
@@ -1,10 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
+import { Home } from './pages/home'
+import { Login } from './pages/login'
+import { Feed } from "./pages/feed";
+import { GlobalStyle } from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+     <GlobalStyle />
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/feed" element={<Feed />} />
+     </Routes >
+    </Router>
   );
 }
 
