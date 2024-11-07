34b513cb2241131d911cf94228594c2cc3255527
Mathx8 <matheusqueirozdev@gmail.com>
2024-11-07 17:52:54 -0300

    Desafio 04 - Login

--- C#Exemplos - Backup/obj/Debug/net8.0/C#Exemplos.AssemblyInfo.cs
+++ C#Exemplos - Backup/obj/Debug/net8.0/C#Exemplos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#Exemplos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8770d9a04f1a61934bdf895e96987c181fb2968d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+030e98d53bb962d032796105ab4db210b445e86b")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#Exemplos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#Exemplos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ desafio04-login/src/components/Button/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+
+export const ButtonContainer = styled.button<{ disabled?: boolean }>`
+    width: 100%;
+    height: 42px;
+    background-color: #81259D;
+    color: #FFF;
+
+    border: 1px solid #81259D;
+    border-radius: 21px;
+
+    &:hover {
+        opacity: 0.6;
+        cursor:pointer;
+    }
+
+    opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
+    pointer-events: ${({ disabled }) => (disabled ? "none" : "auto")};
+`
